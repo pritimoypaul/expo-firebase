8c78c10afdf4552438097f8685f832aa16871cf4
Pritimoy Paul <pritimoy97@gmail.com>
2024-04-25 17:59:04 +0600

    update, delete added

--- screens/home.js
+++ screens/home.js
@@ -1,4 +1,12 @@
-import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  getFirestore,
+  updateDoc,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import {
   ScrollView,
@@ -8,6 +16,7 @@ import {
   View,
 } from "react-native";
 import { firebaseApp } from "../firebaseConfig";
+import { AntDesign } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const db = getFirestore(firebaseApp);
@@ -44,6 +53,20 @@ const HomeScreen = () => {
     setText("");
   };
 
+  const updateData = async (id, isDone) => {
+    const docRef = doc(db, "dummy", id);
+
+    await updateDoc(docRef, {
+      isDone: !isDone,
+    });
+    getData();
+  };
+
+  const deleteData = async (id) => {
+    await deleteDoc(doc(db, "dummy", id));
+    getData();
+  };
+
   useEffect(() => {
     getData();
   }, []);
@@ -71,9 +94,19 @@ const HomeScreen = () => {
         <View className="mt-4">
           <ScrollView>
             {items?.map((item) => (
-              <View key={item.id} className="bg-slate-50 p-3 my-2 rounded-sm">
-                <Text>{item.text}</Text>
-              </View>
+              <TouchableOpacity
+                key={item.id}
+                onPress={() => updateData(item.id, item.isDone)}
+              >
+                <View className="bg-slate-50 p-3 my-2 rounded-sm flex-row justify-between">
+                  <Text className={item.isDone ? "line-through" : ""}>
+                    {item.text}
+                  </Text>
+                  <TouchableOpacity onPress={() => deleteData(item.id)}>
+                    <AntDesign name="delete" size={18} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
             ))}
           </ScrollView>
         </View>
